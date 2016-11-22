962ed3b48d2fc99d47f008fc38144c23471dd028
acordeonl <acordeonlwebmaster@gmail.com>
2016-11-22 03:48:31 -0500

    good sound while playing fr 18

--- vars.js
+++ vars.js
@@ -2,12 +2,25 @@ var context = new AudioContext();
 var convolver = context.createConvolver();
 var dry = context.createGain();
 var wet = context.createGain();
+var compressor = context.createDynamicsCompressor();
+var trebleVolume = context.createGain();
+var biquadFilter = context.createBiquadFilter();
 var audioGains = [];
 var note = 29;
 var flag = true ;
 
-dry.gain.value = (1.0 - 0.2);
-wet.gain.value = 0.2;
+dry.gain.value = (1.0-0.7);
+wet.gain.value = 0.35;
+trebleVolume.gain.value = 1;
+
+compressor.attack=2 ;
+compressor.release=2 ;
+biquadFilter.type = "lowpass";
+biquadFilter.frequency.value = 7000;
+
 convolver.connect(wet);
-dry.connect(context.destination);
-wet.connect(context.destination);
+trebleVolume.connect(dry) ;
+dry.connect(compressor);
+wet.connect(compressor);
+compressor.connect(biquadFilter);
+biquadFilter.connect(context.destination) ;
