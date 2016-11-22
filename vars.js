90b940018d7054f40083b1cb7f47f0d2ee7bc109
acordeonl <acordeonlwebmaster@gmail.com>
2016-11-21 22:43:54 -0500

    organices element

--- vars.js
+++ vars.js
@@ -1,1 +1,13 @@
 var context = new AudioContext();
+var convolver = context.createConvolver();
+var dry = context.createGain();
+var wet = context.createGain();
+var audioGains = [];
+var note = 29;
+var flag = true ;
+
+dry.gain.value = (1.0 - 0.2);
+wet.gain.value = 0.2;
+convolver.connect(wet);
+dry.connect(context.destination);
+wet.connect(context.destination);
